586dd1880671bda56d2f551fc42da72510d77869
gamingyoda <dymjpn@gmail.com>
2023-10-26 09:11:07 +0900

    try to make code more easy to use

--- dmitriy/try1/try1.ino
+++ dmitriy/try1/try1.ino
@@ -2,56 +2,62 @@
 
 BluetoothSerial SerialBT;
 
-const uint8_t speeds = 40;
+const int outputPin1 = 32;
+const int outputPin2 = 33;
+const int outputPin3 = 26;
+const int outputPin4 = 25;
 
 void setup() {
   SerialBT.begin("ESP32LED");      // Bluetoothシリアルに「ESP32LED」という名前をつけて初期化
   Serial.begin(9600);            // シリアルモニタの初期化
+
+  pinMode(outputPin1, OUTPUT);
+  pinMode(outputPin2, OUTPUT);
+  pinMode(outputPin3, OUTPUT);
+  pinMode(outputPin4, OUTPUT);
 }
 
 void loop() {
-  while(0){
-    if (SerialBT.available()) {      // Bluetoothシリアルに受信したかどうかを調べる
-      char ch = SerialBT.read();     // 受信した文字を得る
-      Serial.println(ch);            // 受信した文字をシリアルモニタに出力
+  if (SerialBT.available()) {      // Bluetoothシリアルに受信したかどうかを調べる
+    char ch = SerialBT.read();     // 受信した文字を得る
+    //Serial.println(ch);            // 受信した文字をシリアルモニタに出力
 
-      analogWrite(32,0);
-      analogWrite(33,0);
-      analogWrite(26,0);
-      analogWrite(25,0);
-
-      if (ch == 'w') {               // 受信した文字が「w」の場合
-        analogWrite(32,speeds);
-        analogWrite(33,0);
-        analogWrite(26,speeds);
-        analogWrite(25,0);
-        delay(1000);
-        break;
-      }
-      else if (ch == 'a') {          // 受信した文字が「a」の場合
-        analogWrite(32,speeds);
-        analogWrite(33,0);
-        analogWrite(26,0);
-        analogWrite(25,0);
-        delay(1000);
-        break;
-      }
-      else if (ch == 's') {          // 受信した文字が「s」の場合
-        analogWrite(32,0);
-        analogWrite(33,speeds);
-        analogWrite(26,0);
-        analogWrite(25,speeds);
-        delay(1000);
-        break;
-      }
-      else if (ch == 'd') {          // 受信した文字が「d」の場合
-        analogWrite(32,0);
-        analogWrite(33,0);
-        analogWrite(26,speeds);
-        analogWrite(25,0);
-        delay(1000);
-        break;
-      }
+    if (ch == 'w') {               // 受信した文字が「w」の場合
+      digitalWrite(outputPin1,HIGH);
+      digitalWrite(outputPin2,LOW);
+      digitalWrite(outputPin3,HIGH);
+      digitalWrite(outputPin4,LOW);
+    }
+    else if (ch == 'a') {          // 受信した文字が「a」の場合
+      digitalWrite(outputPin1,HIGH);
+      digitalWrite(outputPin2,LOW);
+      digitalWrite(outputPin3,LOW);
+      digitalWrite(outputPin4,LOW);
+    }
+    else if (ch == 's') {          // 受信した文字が「s」の場合
+      digitalWrite(outputPin1,LOW);
+      digitalWrite(outputPin2,HIGH);
+      digitalWrite(outputPin3,LOW);
+      digitalWrite(outputPin4,HIGH);
+    }
+    else if (ch == 'd') {          // 受信した文字が「d」の場合
+      digitalWrite(outputPin1,LOW);
+      digitalWrite(outputPin2,LOW);
+      digitalWrite(outputPin3,HIGH);
+      digitalWrite(outputPin4,LOW);
+    }
+    else{
+      digitalWrite(outputPin1,LOW);
+      digitalWrite(outputPin2,LOW);
+      digitalWrite(outputPin3,LOW);
+      digitalWrite(outputPin4,LOW);
     }
   }
+  else{
+    digitalWrite(outputPin1,LOW);
+    digitalWrite(outputPin2,LOW);
+    digitalWrite(outputPin3,LOW);
+    digitalWrite(outputPin4,LOW);
+  }
+
 }
--- dmitriy/try1/try1.py
+++ dmitriy/try1/try1.py
@@ -1,5 +1,6 @@
 import serial 
-#import time
+import serial.tools.list_ports
+import time
 import pygame
 
 #setup for controller
@@ -10,11 +11,17 @@ joystick = pygame.joystick.Joystick(0)
 joystick.init()
 
 #setup for esp32 
-'''
-Change "COM9" to Serial port witch conected to your computer.
-You can check this from tera turm.
-'''
-ser = serial.Serial("COM9", 9600)
+
+# ser = serial.Serial()
+# ser.baudrate = 9600
+# devices = serial.tools.list_ports.comports()
+# port_found = False
+
+# for device in devices:
+#     if device.usb_description()=='ESP32LED':
+#         ser.port=device[0]
+#         port_found = True
+
 
 #Main program
 done = False
@@ -30,69 +37,99 @@ while not done:
         #button
         if 1==joystick.get_button(0): # Green A button
             ser.write(b'0')
+            time.sleep(0.1)
+            print("A")
         if 1==joystick.get_button(1): # Red B button
             ser.write(b'1')
+            time.sleep(0.1)
+            print("B")
         if 1==joystick.get_button(2): # Blue X button
             ser.write(b'2')
+            time.sleep(0.1)
         if 1==joystick.get_button(3): # Yellow Y button
             ser.write(b'3')
+            time.sleep(0.1)
         if 1==joystick.get_button(4): # LB button
             ser.write(b'4')
+            time.sleep(0.1)
         if 1==joystick.get_button(5): # RB button
             ser.write(b'5')
+            time.sleep(0.1)
         if 0.7<joystick.get_axis(4):  # LT button
             ser.write(b'r')
+            time.sleep(0.1)
         if 0.7<joystick.get_axis(5):  # RT button
             ser.write(b'l')
+            time.sleep(0.1)
         if 1==joystick.get_button(6): # BACK button
             ser.write(b'6')
+            time.sleep(0.1)
         if 1==joystick.get_button(7): # START button
             ser.write(b'7')
+            time.sleep(0.1)
         if 1==joystick.get_button(8): # Left stick button
             ser.write(b'8')
+            time.sleep(0.1)
         if 1==joystick.get_button(9): # Right stick button
             ser.write(b'9')
+            time.sleep(0.1)
 
         #Left Stick
         if -0.4>joystick.get_axis(1): # Left Stick Y axis + 
-            ser.write(b'w') 
+            ser.write(b'w')
+            time.sleep(0.1) 
         if -0.4>joystick.get_axis(0): # Left Stick X axis - 
             ser.write(b'a')
+            time.sleep(0.1)
         if 0.4<joystick.get_axis(1): # Left Stick Y axis - 
             ser.write(b's')
+            time.sleep(0.1)
         if 0.4<joystick.get_axis(0): # Left Stick X axis + 
             ser.write(b'd')
+            time.sleep(0.1)
 
         #Right Stick
         if -0.4>joystick.get_axis(3): # Right Stick Y axis +
             ser.write(b'u')
+            time.sleep(0.1)
         if -0.4>joystick.get_axis(2): # Right Stick X axis -
-            ser.write(b'h') 
+            ser.write(b'h')
+            time.sleep(0.1) 
         if 0.4<joystick.get_axis(3): # Right Stick Y axis - 
             ser.write(b'j')
+            time.sleep(0.1)
         if 0.4<joystick.get_axis(2): # Right Stick X axis + 
             ser.write(b'k')
+            time.sleep(0.1)
 
         #Cross Button ( front = north )
         if (0,1)==joystick.get_hat(0): # North
-            ser.write(b'N')
-        if (1,1)==joystick.get_hat(0): # Northeast
-            ser.write(b'z')
+            ser.write(b'w')
+            time.sleep(0.1)
+        # if (1,1)==joystick.get_hat(0): # Northeast
+        #     ser.write(b'z')
+        #     time.sleep(0.1)
         if (1,0)==joystick.get_hat(0): # East
-            ser.write(b'E')
-        if (1,-1)==joystick.get_hat(0): # Southeast
-            ser.write(b'x')
+            ser.write(b'd')
+            time.sleep(0.1)
+        # if (1,-1)==joystick.get_hat(0): # Southeast
+        #     ser.write(b'x')
+        #     time.sleep(0.1)
         if (0,-1)==joystick.get_hat(0): # South
-            ser.write(b'S')
-        if (-1,-1)==joystick.get_hat(0): # Southwest
-            ser.write(b'c')
+            ser.write(b's')
+            time.sleep(0.1)
+        # if (-1,-1)==joystick.get_hat(0): # Southwest
+        #     ser.write(b'c')
+        #     time.sleep(0.1)
         if (-1,0)==joystick.get_hat(0): # West
-            ser.write(b'W')
-        if (-1,1)==joystick.get_hat(0): # Northwest
-            ser.write(b'v')
+            ser.write(b'a')
+            time.sleep(0.1)
+        # if (-1,1)==joystick.get_hat(0): # Northwest
+        #     ser.write(b'v')
+        #     time.sleep(0.1)
 
 #ser.write(b'0')
-#time.sleep(10)
+time.sleep(10)
 #ser.write(b'1')
 
 #ser.close()
