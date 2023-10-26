973f3ec62a1e0eb840c9852277c854e2d9a9cda8
gamingyoda <dymjpn@gmail.com>
2023-10-27 03:44:52 +0900

    a

--- dmitriy/try1/try1.py
+++ dmitriy/try1/try1.py
@@ -28,100 +28,79 @@ while not done:
 
         #button
         if 1==joystick.get_button(0): # Green A button
-            ser.write(b'0')
-            time.sleep(0.1)
+            ser.write(b'0')            
             print("A")
         if 1==joystick.get_button(1): # Red B button
             ser.write(b'1')
-            time.sleep(0.1)
             print("B")
         if 1==joystick.get_button(2): # Blue X button
             ser.write(b'2')
-            time.sleep(0.1)
         if 1==joystick.get_button(3): # Yellow Y button
             ser.write(b'3')
-            time.sleep(0.1)
         if 1==joystick.get_button(4): # LB button
             ser.write(b'4')
-            time.sleep(0.1)
         if 1==joystick.get_button(5): # RB button
             ser.write(b'5')
-            time.sleep(0.1)
         if 0.7<joystick.get_axis(4):  # LT button
             ser.write(b'r')
-            time.sleep(0.1)
         if 0.7<joystick.get_axis(5):  # RT button
             ser.write(b'l')
-            time.sleep(0.1)
         if 1==joystick.get_button(6): # BACK button
             ser.write(b'6')
-            time.sleep(0.1)
         if 1==joystick.get_button(7): # START button
             ser.write(b'7')
-            time.sleep(0.1)
         if 1==joystick.get_button(8): # Left stick button
             ser.write(b'8')
-            time.sleep(0.1)
         if 1==joystick.get_button(9): # Right stick button
             ser.write(b'9')
-            time.sleep(0.1)
+            
 
         #Left Stick
         if -0.4>joystick.get_axis(1): # Left Stick Y axis + 
             ser.write(b'w')
-            time.sleep(0.1) 
         if -0.4>joystick.get_axis(0): # Left Stick X axis - 
             ser.write(b'a')
-            time.sleep(0.1)
         if 0.4<joystick.get_axis(1): # Left Stick Y axis - 
             ser.write(b's')
-            time.sleep(0.1)
         if 0.4<joystick.get_axis(0): # Left Stick X axis + 
             ser.write(b'd')
-            time.sleep(0.1)
+            
 
         #Right Stick
         if -0.4>joystick.get_axis(3): # Right Stick Y axis +
             ser.write(b'u')
-            time.sleep(0.1)
         if -0.4>joystick.get_axis(2): # Right Stick X axis -
             ser.write(b'h')
-            time.sleep(0.1) 
         if 0.4<joystick.get_axis(3): # Right Stick Y axis - 
             ser.write(b'j')
-            time.sleep(0.1)
         if 0.4<joystick.get_axis(2): # Right Stick X axis + 
             ser.write(b'k')
-            time.sleep(0.1)
+            
 
         #Cross Button ( front = north )
         if (0,1)==joystick.get_hat(0): # North
             ser.write(b'w')
-            time.sleep(0.1)
+                
         # if (1,1)==joystick.get_hat(0): # Northeast
         #     ser.write(b'z')
-        #     time.sleep(0.1)
+        #     
         if (1,0)==joystick.get_hat(0): # East
             ser.write(b'd')
-            time.sleep(0.1)
         # if (1,-1)==joystick.get_hat(0): # Southeast
         #     ser.write(b'x')
-        #     time.sleep(0.1)
+        #     
         if (0,-1)==joystick.get_hat(0): # South
             ser.write(b's')
-            time.sleep(0.1)
         # if (-1,-1)==joystick.get_hat(0): # Southwest
         #     ser.write(b'c')
-        #     time.sleep(0.1)
+        #     
         if (-1,0)==joystick.get_hat(0): # West
             ser.write(b'a')
-            time.sleep(0.1)
         # if (-1,1)==joystick.get_hat(0): # Northwest
         #     ser.write(b'v')
-        #     time.sleep(0.1)
+        #     
 
 #ser.write(b'0')
 time.sleep(10)
 #ser.write(b'1')
-
-#ser.close()
+ser.close()
