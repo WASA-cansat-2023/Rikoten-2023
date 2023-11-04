32df07a49bff1c7936d072515d9e95149c115d49
Koya Saka <k7oya14@gmail.com>
2023-11-04 16:59:52 +0900

    fix delay and add stop() func

--- dmitriy/master/master.ino
+++ dmitriy/master/master.ino
@@ -1,35 +1,27 @@
 #include <BluetoothSerial.h>
 
-#define outputPin1 32
-#define outputPin2 33
-#define outputPin3 26
-#define outputPin4 25
+#define outputPin0 32
+#define outputPin1 33
+#define outputPin2 26
+#define outputPin3 25
 
 BluetoothSerial SerialBT;
 
-void forward(uint32_t pwm);
-void backward(uint32_t pwm);
-void left(uint32_t pwm);
-void right(uint32_t pwm);
+void forward(void);
+void backward(void);
+void left(void);
+void right(void);
+void stop(void);
+void nothing(void);
 
 void setup() {
   SerialBT.begin("ESP32_1");      // Bluetoothシリアルに「ESP32_1」という名前をつけて初期化 // 2つ目の機体は「ESP32_2」に変更
   Serial.begin(115200);            
 
+  pinMode(outputPin0, OUTPUT);
   pinMode(outputPin1, OUTPUT);
-  pinMode(outputPin1, OUTPUT);
-  pinMode(outputPin1, OUTPUT);
-  pinMode(outputPin1, OUTPUT);
-
-  ledcSetup(0, 5000, 8);
-  ledcSetup(1, 5000, 8);
-  ledcSetup(2, 5000, 8);
-  ledcSetup(3, 5000, 8);
-
-  ledcAttachPin(outputPin1, 0);
-  ledcAttachPin(outputPin2, 1);
-  ledcAttachPin(outputPin3, 2);
-  ledcAttachPin(outputPin4, 3);
+  pinMode(outputPin2, OUTPUT);
+  pinMode(outputPin3, OUTPUT);
 }
 
 void loop() {
@@ -39,60 +31,64 @@ void loop() {
 
 
     if (ch == '4') {               // 受信した文字が「0」の場合
-      forward(250);
-      delay(1000);
+      forward();
     }
     else if (ch == '5') {          // 受信した文字が「1」の場合
-      backward(250);
-      delay(1000);
+      backward();
     }
     else if (ch == '6') {          // 受信した文字が「1」の場合
-      left(250);
-      delay(1000);
+      left();
     }
     else if (ch == '7') {          // 受信した文字が「1」の場合
-      right(250);
-      delay(1000);
+      right();
     }
     else{
-      ledcWrite(0,0);
-      ledcWrite(1,0);
-      ledcWrite(2,0);
-      ledcWrite(3,0);
+      stop();
     }
-  }
-  else{
-    ledcWrite(0,0);
-    ledcWrite(1,0);
-    ledcWrite(2,0);
-    ledcWrite(3,0);
+  }else{
+    delay(10);
+    stop();
   }
 }
 
-void forward(uint32_t pwm) {
-  ledcWrite(0,pwm);
-  ledcWrite(1,0);
-  ledcWrite(2,pwm);
-  ledcWrite(3,0);
+void forward(void) {
+  digitalWrite(outputPin0,1);
+  digitalWrite(outputPin1,0);
+  digitalWrite(outputPin2,1);
+  digitalWrite(outputPin3,0);
 }
 
-void backward(uint32_t pwm) {
-  ledcWrite(0, 0);
-  ledcWrite(1, pwm);
-  ledcWrite(2, 0);
-  ledcWrite(3, pwm);
+void backward(void) {
+  digitalWrite(outputPin0, 0);
+  digitalWrite(outputPin1, 1);
+  digitalWrite(outputPin2, 0);
+  digitalWrite(outputPin3, 1);
 }
 
-void left(uint32_t pwm) {
-  ledcWrite(0, pwm);
-  ledcWrite(1, 0);
-  ledcWrite(2, 0);
-  ledcWrite(3, 0);
+void left(void) {
+  digitalWrite(outputPin0, 1);
+  digitalWrite(outputPin1, 0);
+  digitalWrite(outputPin2, 0);
+  digitalWrite(outputPin3, 1);
 }
 
-void right(uint32_t pwm) {
-  ledcWrite(0, 0);
-  ledcWrite(1, 0);
-  ledcWrite(2, pwm);
-  ledcWrite(3, 0);
+void right(void) {
+  digitalWrite(outputPin0, 0);
+  digitalWrite(outputPin1, 1);
+  digitalWrite(outputPin2, 1);
+  digitalWrite(outputPin3, 0);
+}
+
+void stop(void){
+  digitalWrite(outputPin0, 1);
+  digitalWrite(outputPin1, 1);
+  digitalWrite(outputPin2, 1);
+  digitalWrite(outputPin3, 1);
+}
+
+void nothing(void){
+  digitalWrite(outputPin0, 0);
+  digitalWrite(outputPin1, 0);
+  digitalWrite(outputPin2, 0);
+  digitalWrite(outputPin3, 0);
 }
